87c98ff0a3906ab6396b0fa3d7d98f2f98dae91b
Rustam Iunusov <Rustam_Iunusov@epam.com>
2017-10-13 17:17:22 +0300

    Begined Lab_5

---
+++ Lab_5/main.py
@@ -0,0 +1,8 @@
+
+
+if __name__ == '__main__':
+    n, q = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+
+    for _ in range(q):
+        c, l, r = list(map(int, input().split()))
